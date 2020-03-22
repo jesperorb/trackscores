7fcf8b518cea8b2c00b84e0f1cc174a40d9772ca
jesperorb <jesperorb@gmail.com>
2020-03-22 08:00:15 +0100

    Create records from snapshots

--- utils/hooks.ts
+++ utils/hooks.ts
@@ -1,10 +1,10 @@
 import { useFirestore, useFirestoreCollection } from "reactfire";
 
 import { matchesCollection, usersCollection, sessionsCollection } from "../config";
-import { Match } from "../models/match";
-import { Session } from "../models/session";
+import { Match, MatchValues } from "../models/match";
+import { Session, SessionValues } from "../models/session";
 import { querySnapshotToRecords } from "./";
-import { User } from "../models/user";
+import { User, UserValues } from "../models/user";
 
 export type FirebaseAppData = {
   sessions: Session[];
@@ -16,15 +16,24 @@ export type FirebaseAppData = {
 export function useFirebaseAppData(): FirebaseAppData {
   const matchesRef = useFirestore().collection(matchesCollection);
   const matchesSnapshot = useFirestoreCollection(matchesRef);
-  const matchRecords = querySnapshotToRecords<Match>(matchesSnapshot);
+  const matchRecords = querySnapshotToRecords<Match, MatchValues>(
+    <firebase.firestore.QuerySnapshot>(<unknown>matchesSnapshot),
+    Match
+  );
 
   const usersRef = useFirestore().collection(usersCollection);
   const usersSnapshot = useFirestoreCollection(usersRef);
-  const userRecords = querySnapshotToRecords<User>(usersSnapshot);
+  const userRecords = querySnapshotToRecords<User, UserValues>(
+    <firebase.firestore.QuerySnapshot>(<unknown>usersSnapshot),
+    User
+  );
 
   const sessionsRef = useFirestore().collection(sessionsCollection);
   const sessionsSnapshot = useFirestoreCollection(sessionsRef);
-  const sessionRecords = querySnapshotToRecords<Session>(sessionsSnapshot);
+  const sessionRecords = querySnapshotToRecords<Session, SessionValues>(
+    <firebase.firestore.QuerySnapshot>(<unknown>sessionsSnapshot),
+    Session
+  );
 
   return {
     sessions: sessionRecords,
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,12 @@
-export const querySnapshotToRecords = <T extends object>(snapshot: any): T[] => {
-  return snapshot.docs.map((doc: any) => Object.assign({}, { id: doc.id, ...doc.data() }));
+
+export type Newable<T, K> = { new (values: K): T; }
+
+export const querySnapshotToRecords = <
+  T extends object,
+  K extends object
+>(snapshot: firebase.firestore.QuerySnapshot, Record: Newable<T, K>): T[] => {
+  return snapshot.docs.map((doc) => {
+    const data = <Partial<K>>doc?.data();
+    return new Record(<K>{ id: doc.id, ...data });
+  });
 }
